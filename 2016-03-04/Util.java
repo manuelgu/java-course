bd7913e09df3647815be4e346ffeb51d940f603d
manuelgu <manuelgu@users.noreply.github.com>
2016-03-11 13:57:11 +0100

    Update 2016-03-04

--- 2016-03-04/Util.java
+++ 2016-03-04/Util.java
@@ -5,5 +5,7 @@ public class Util {
     public static final char AIR = '.';
     public static final char TRAPDOOR = 'T';
     public static final char PLAYER = 'ß';
+    
+    public static final String RAN_INTO_WAL = "Whoops, you ran into a wall. That hurt, didn't it?";
 
 }
